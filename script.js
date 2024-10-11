22f522b147206e999f4a4869752a551e458835a5
xelavi <xelavi123@gmail.com>
2024-10-12 01:09:40 +0200

    eliminar basura

--- script.js
+++
@@ -1,8 +0,0 @@
-function buildGridFromJSON(data) {
-    data.nodes.forEach(node => {
-        const [row, col] = node.position;
-        grid[row][col] = { shape: node.shape, text: node.text };
-    });
-    links = data.links || [];
-    createGrid();
-}
